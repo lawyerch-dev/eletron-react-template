b2477ea370c24a566698caaf7afb2f7a67a8f207
BluerAngala <1107117864@qq.com>
2026-08-24 14:13:31 +0800

    refactor: 更新vitest配置使用ES模块语法并优化E2E测试排除规则

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,9 +2,9 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    root: __dirname,
+    root: import.meta.dirname,
     include: ['test/**/*.{test,spec}.?(c|m)[jt]s?(x)'],
-    exclude: ['test/e2e.spec.ts'],
+    exclude: ['test/e2e/**'],
     passWithNoTests: true,
     testTimeout: 1000 * 29,
   },
